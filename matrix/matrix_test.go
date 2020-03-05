a669019b99f4ea1c6366c124a4e6baa10bce4d1b
Maciej Tarnowski <maciej@tarnowski.it>
2020-03-05 22:35:40 +0100

    Test inverting more matrices

--- matrix/matrix_test.go
+++ matrix/matrix_test.go
@@ -191,3 +191,23 @@ func TestInvertingMatrix(t *testing.T) {
 	assert.InDelta(t, float64(105)/float64(532), inverted.At(2, 3), 0.00001)
 	assert.True(t, inverted.Equals(expected))
 }
+
+func TestInvertingMatrices(t *testing.T) {
+	tests := []struct {
+		matrix   *Matrix
+		expected *Matrix
+	}{
+		{
+			NewMatrix(4, 4, 8, -5, 9, 2, 7, 5, 6, 1, -6, 0, 9, 6, -3, 0, -9, -4),
+			NewMatrix(4, 4, -0.15385, -0.15385, -0.28205, -0.53846, -0.07692, 0.12308, 0.02564, 0.03077, 0.35897, 0.35897, 0.43590, 0.92308, -0.69231, -0.69231, -0.76923, -1.92308),
+		},
+		{
+			NewMatrix(4, 4, 9, 3, 0, 9, -5, -2, -6, -3, -4, 9, 6, 4, -7, 6, 6, 2),
+			NewMatrix(4, 4, -0.04074, -0.07778, 0.14444, -0.22222, -0.07778, 0.03333, 0.36667, -0.33333, -0.02901, -0.14630, -0.10926, 0.12963, 0.17778, 0.06667, -0.26667, 0.33333),
+		},
+	}
+
+	for _, tt := range tests {
+		assert.True(t, tt.matrix.Invert().Equals(tt.expected))
+	}
+}
