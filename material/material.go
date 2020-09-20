6fcae22cb5600f900924332911584d28524af91c
Maciej Tarnowski <maciej@tarnowski.it>
2020-09-20 17:31:25 +0200

    Light and Shading - implement Phong reflection model

--- material/material.go
+++ material/material.go
@@ -1,6 +1,11 @@
 package material
 
-import "goray/color"
+import (
+	"goray/color"
+	"goray/light"
+	"goray/tuple"
+	"math"
+)
 
 type Material struct {
 	Color     *color.Color
@@ -12,4 +17,35 @@ type Material struct {
 
 func NewMaterial() *Material {
 	return &Material{Color: color.NewColor(1, 1, 1), Ambient: 0.1, Diffuse: 0.9, Specular: 0.9, Shininess: 200.0}
+}
+
+func (m *Material) Lighting(l *light.Light, point *tuple.Tuple, eyeV *tuple.Tuple, normalV *tuple.Tuple) *color.Color {
+	effectiveColor := m.Color.Multiply(l.Intensity)
+
+	lightV := l.Position.Sub(point).Normalize()
+
+	ambient := effectiveColor.MultiplyScalar(m.Ambient)
+
+	lightDotNormal := lightV.Dot(normalV)
+
+	var diffuse *color.Color
+	var specular *color.Color
+	if lightDotNormal < 0 {
+		diffuse = color.NewColor(0, 0, 0)
+		specular = color.NewColor(0, 0, 0)
+	} else {
+		diffuse = effectiveColor.MultiplyScalar(m.Diffuse).MultiplyScalar(lightDotNormal)
+
+		reflectV := lightV.Negate().Reflect(normalV)
+		reflectDotEye := reflectV.Dot(eyeV)
+
+		if reflectDotEye <= 0 {
+			specular = color.NewColor(0, 0, 0)
+		} else {
+			factor := math.Pow(reflectDotEye, m.Shininess)
+			specular = l.Intensity.MultiplyScalar(m.Specular).MultiplyScalar(factor)
+		}
+	}
+
+	return ambient.Add(diffuse).Add(specular)
 }
--- material/material_test.go
+++ material/material_test.go
@@ -3,6 +3,9 @@ package material
 import (
 	"github.com/stretchr/testify/assert"
 	"goray/color"
+	"goray/light"
+	"goray/tuple"
+	"math"
 	"testing"
 )
 
@@ -14,4 +17,69 @@ func TestDefaultMaterial(t *testing.T) {
 	assert.Equal(t, 0.9, m.Diffuse)
 	assert.Equal(t, 0.9, m.Specular)
 	assert.Equal(t, 200.0, m.Shininess)
+}
+
+func TestLightingWithEyeBetweenLightAndSurface(t *testing.T) {
+	m := NewMaterial()
+	position := tuple.NewPoint(0, 0, 0)
+
+	eyeV := tuple.NewVector(0, 0, -1)
+	normalV := tuple.NewVector(0, 0, -1)
+	l := light.NewPointLight(tuple.NewPoint(0, 0, -10), color.NewColor(1, 1, 1))
+
+	result := m.Lighting(l, position, eyeV, normalV)
+
+	assert.Equal(t, color.NewColor(1.9, 1.9, 1.9), result)
+}
+
+func TestLightingWithEyeBetweenLightAndSurfaceWithEyeOffset45Degrees(t *testing.T) {
+	m := NewMaterial()
+	position := tuple.NewPoint(0, 0, 0)
+
+	eyeV := tuple.NewVector(0, math.Sqrt(2)/2, -math.Sqrt(2)/2)
+	normalV := tuple.NewVector(0, 0, -1)
+	l := light.NewPointLight(tuple.NewPoint(0, 0, -10), color.NewColor(1, 1, 1))
+
+	result := m.Lighting(l, position, eyeV, normalV)
+
+	assert.Equal(t, color.NewColor(1.0, 1.0, 1.0), result)
+}
+
+func TestLightingWithEyeOppositeSurfaceWithLightOffset45Degrees(t *testing.T) {
+	m := NewMaterial()
+	position := tuple.NewPoint(0, 0, 0)
+
+	eyeV := tuple.NewVector(0, 0, -1)
+	normalV := tuple.NewVector(0, 0, -1)
+	l := light.NewPointLight(tuple.NewPoint(0, 10, -10), color.NewColor(1, 1, 1))
+
+	result := m.Lighting(l, position, eyeV, normalV)
+
+	assert.True(t, result.Equals(color.NewColor(0.7364, 0.7364, 0.7364)))
+}
+
+func TestLightingWithEyeInPathOfReflectionVector(t *testing.T) {
+	m := NewMaterial()
+	position := tuple.NewPoint(0, 0, 0)
+
+	eyeV := tuple.NewVector(0, -math.Sqrt(2)/2, -math.Sqrt(2)/2)
+	normalV := tuple.NewVector(0, 0, -1)
+	l := light.NewPointLight(tuple.NewPoint(0, 10, -10), color.NewColor(1, 1, 1))
+
+	result := m.Lighting(l, position, eyeV, normalV)
+
+	assert.True(t, result.Equals(color.NewColor(1.6364, 1.6364, 1.6364)))
+}
+
+func TestLightingWithLightBehindSurface(t *testing.T) {
+	m := NewMaterial()
+	position := tuple.NewPoint(0, 0, 0)
+
+	eyeV := tuple.NewVector(0, 0, -1)
+	normalV := tuple.NewVector(0, 0, -1)
+	l := light.NewPointLight(tuple.NewPoint(0, 0, 10), color.NewColor(1, 1, 1))
+
+	result := m.Lighting(l, position, eyeV, normalV)
+
+	assert.True(t, result.Equals(color.NewColor(0.1, 0.1, 0.1)))
 }
